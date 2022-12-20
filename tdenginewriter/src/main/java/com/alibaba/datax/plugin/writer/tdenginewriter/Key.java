6d70aec9a1f7c065282d5310fc6931435ca4b76c
yuanpai.zhang <yuanpai.zhang@newratetech.com>
2022-12-20 11:19:37 +0800

    [TS-2293]<enh>add fetch size.

--- tdenginewriter/src/main/java/com/alibaba/datax/plugin/writer/tdenginewriter/Key.java
+++ tdenginewriter/src/main/java/com/alibaba/datax/plugin/writer/tdenginewriter/Key.java
@@ -4,6 +4,7 @@ public class Key {
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
     public static final String CONNECTION = "connection";
+    public static final String FETCH_SIZE = "fetchSize";
     public static final String BATCH_SIZE = "batchSize";
     public static final String TABLE = "table";
     public static final String JDBC_URL = "jdbcUrl";
